69faebd0570f08691b1f910363ca985ba34b6be7
gbox3d <gbox3d@gmail.com>
2021-06-20 12:06:07 +0900

    install nodejs current

--- ubuntu/install_nodejs.sh
+++ ubuntu/install_nodejs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
 # Using Ubuntu for 15
-curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+#curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
